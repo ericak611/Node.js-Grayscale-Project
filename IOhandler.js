726bbcfc80c9b19bfa8b8d97cced1f17b68ebd28
Hyewon Kim <hkim444@my.bcit.ca>
2023-02-25 16:01:34 -0800

    added grayScale function in IOhandler.js

--- IOhandler.js
+++ IOhandler.js
@@ -40,4 +40,43 @@ const unzip = (pathIn, pathOut) => {
 const readDir = (dir) => {
     return fsp.readdir(dir)
       .then((all_files) =>  all_files.filter(png => path.extname(png)));
-  };
+};
+
+/**
+ * Description: Read in png file by given pathIn,
+ * convert to grayscale and write to given pathOut
+ *
+ * @param {string} filePath
+ * @param {string} pathProcessed
+ * @return {promise}
+ */
+const grayScale = (pathIn, pathOut) => {
+    fs.createReadStream(pathIn)
+    .pipe(
+      new PNG({
+        filterType: 4,
+      })
+    )
+    .on("parsed", function () {
+      for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+          var idx = (this.width * y + x) << 2;
+
+          // invert color
+          this.data[idx] = 255 - this.data[idx];
+          this.data[idx + 1] = 255 - this.data[idx + 1];
+          this.data[idx + 2] = 255 - this.data[idx + 2];
+
+          // and reduce opacity
+          this.data[idx + 3] = this.data[idx + 3] >> 1;
+        }
+      }
+      this.pack().pipe(fs.createWriteStream(pathOut));
+    });
+};
+
+module.exports = {
+unzip,
+readDir,
+grayScale,
+};
